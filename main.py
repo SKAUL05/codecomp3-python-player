5eeb89fa5439256dabee37701ef60bdc57c68099
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-07 06:10:05 +0000

    Codecomp Python Setup Added

---
+++ main.py
@@ -0,0 +1,20 @@
+import datetime
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
+
+    return render_template('index.html', times=dummy_times)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
