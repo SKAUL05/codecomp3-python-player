5a5de64b7ef0a3853946de86043af9d984b23e00
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-08 05:26:48 +0000

    TF Provider Changes

--- src_tf/provider-main.tf
+++ src_tf/provider-main.tf
@@ -6,9 +6,3 @@ terraform {
     credentials = "service_account.json"
   }
 }
-provider "google" {
-  credentials = file(var.credentials_file_path)
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
-}
