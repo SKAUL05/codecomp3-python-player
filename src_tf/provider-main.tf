282681bf805b1398e056631aa5c99289fefb6a61
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-08 05:54:26 +0000

    TF Provider Changes

--- src_tf/provider-main.tf
+++ src_tf/provider-main.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = "~> 0.12.6"
+  required_version = ">= 0.12.6"
   backend "gcs" {
-    bucket = "terraform-271405-infra-tf-state"
+    bucket = "infra-tf-state-terraform-271405"
     prefix = "cc3"
     credentials = "service_account.json"
   }
