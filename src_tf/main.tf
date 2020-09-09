2c76d2ba30b0cb9a77b67a3af925e22e09f78a2b
SARATH KAUL <kaul.sarath@gmail.com>
2020-09-09 23:32:02 +0530

    CloudBuild Changes

--- src_tf/main.tf
+++ src_tf/main.tf
@@ -16,8 +16,6 @@ resource "google_project_service" "project" {
 }
 
 
-
-
 resource "google_project_iam_binding" "project" {
      project = var.project
      role = "roles/editor"
@@ -25,31 +23,6 @@ resource "google_project_iam_binding" "project" {
       "serviceAccount:409616791234@cloudbuild.gserviceaccount.com",
     ]
 }
-
-resource "google_cloudbuild_trigger" "codecomp-trigger" {
-  provider = google-beta
-  name =  "codecomp-trigger"
-  description = "Github Codecomp Trigger"
-  github {
-    owner = "SKAUL05"
-    name  = "codecomp3-python-player"
-    push {
-      branch = ".*"
-    }
-  }
-  
-  substitutions = {
-      _PROJECT_ID = "codecomp-dev-skaul05"
-    }
-
-  filename = "cloudbuild.yaml"
-  depends_on = [
-    google_project_service.project[5],
-	google_project_service.project[1],
-
-  ]
-}
-
 
 provider "google-beta" {
   credentials = file(var.credentials_file_path)
