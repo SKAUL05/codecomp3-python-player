a354db6a0ef5532fb14994088e84679dd8187cdb
SARATH KAUL <kaul.sarath@gmail.com>
2020-09-10 10:46:23 +0530

    TF Commands added in cloudbuild

--- src_tf/main.tf
+++ src_tf/main.tf
@@ -15,16 +15,29 @@ resource "google_project_service" "project" {
 }
 
 
-resource "google_project_iam_binding" "project" {
-     project = var.project
-     role = "roles/owner"
-	 members = [
-      "serviceAccount:409616791234@cloudbuild.gserviceaccount.com",
-    ]
+resource "google_cloudbuild_trigger" "codecomp-trigger" {	
+  provider = google-beta	
+  name =  "codecomp-trigger-sk"	
+  description = "Github Codecomp Trigger"	
+  github {	
+    owner = "SKAUL05"	
+    name  = "codecomp3-python-player"	
+    push {	
+      branch = ".*"	
+    }	
+  }	
+
+  substitutions = {	
+      _PROJECT_ID = "codecomp-dev-skaul05"	
+    }	
+
+  filename = "cloudbuild.yaml"	
+  depends_on = [	
+    google_project_service.project[5],	
+	google_project_service.project[1],	
+
+  ]	
 }
-
-
-
 
 provider "google-beta" {
   project     = var.project
